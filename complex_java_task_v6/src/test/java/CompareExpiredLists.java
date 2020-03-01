97a8aec86c1d9c781842685386ec39503c124292
Mykhailo Tsalan <pmtsmichael@gmail.com>
2020-03-01 18:05:29 +0200

    created testng test compareExpiredLists

--- complex_java_task_v6/src/test/java/CompareExpiredLists.java
+++ complex_java_task_v6/src/test/java/CompareExpiredLists.java
@@ -1,2 +1,34 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+
 public class CompareExpiredLists {
+
+    @Test
+    public void compareExpiredFoodProducts(){
+
+        Product example1 = new FoodProduct("ChinaFood","FriedDogChicks","10/02/2020","FastFood",7);
+        Product example2 = new FoodProduct("Yamaika.Transition", "yellow-yellow", "12/12/2019", "ice-banana",90);
+        Product example3 = new FoodProduct("Korea", "spice", "05/10/2018", "hot cake", 25);
+        Product example4 = new FoodProduct("NashaRyaba", "chicken legs", "01/02/2020", "raw food", 100);
+
+        ArrayList<Product> listForTest = new ArrayList<>();
+        ArrayList<Product> expectedList = new ArrayList<>();
+
+        listForTest.add(example1);
+        listForTest.add(example2);
+        listForTest.add(example3);
+        listForTest.add(example4);
+
+        expectedList.add(example1);
+        expectedList.add(example2);
+        expectedList.add(example3);
+
+        ArrayList<Product> actualList = Expired.findExpiredFoodProducts(listForTest);
+
+        Assert.assertEquals(actualList,expectedList);
+    }
+
+
 }
