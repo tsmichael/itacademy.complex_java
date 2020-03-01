3608f28968a975490be8fffe2a7dd1861f153cb2
Mykhailo Tsalan <pmtsmichael@gmail.com>
2020-03-01 17:30:24 +0200

    created 1st testng test . getAge method

---
+++ complex_java_task_v6/src/test/java/UnitTests.java
@@ -0,0 +1,15 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class UnitTests {
+
+    @Test
+    public void test_getAge(){
+
+        Product example1 = new Product("Apple.inc", "iPhone X", "10/07/2005");
+
+        int age_tested = example1.getAge();
+
+        Assert.assertEquals(age_tested, 14);
+    }
+}
