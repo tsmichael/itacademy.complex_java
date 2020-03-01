73ff1b4e36b65fc4b480e0dd8dce5a2bada57097
Mykhailo Tsalan <pmtsmichael@gmail.com>
2020-03-01 23:51:01 +0200

    try to add Before method but i delay this idea

--- complex_java_task_v6/src/test/java/CompareAgeInYears.java
+++ complex_java_task_v6/src/test/java/CompareAgeInYears.java
@@ -1,7 +1,7 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class compareAgeInYears {
+public class CompareAgeInYears {
 
     @Test
     public void test_getAge(){
--- complex_java_task_v6/src/test/java/CompareSortingOrder.java
+++ complex_java_task_v6/src/test/java/CompareSortingOrder.java
@@ -15,12 +15,14 @@ public class CompareSortingOrder {
         Product example4 = new FoodProduct("NashaRyaba", "chicken legs", "01/02/2020", "raw food", 100);
 
         ArrayList<Product> listForTest = new ArrayList<>();
-        ArrayList<Product> expectedList = new ArrayList<>();
 
         listForTest.add(example1);
         listForTest.add(example2);
         listForTest.add(example3);
         listForTest.add(example4);
+
+        ArrayList<Product> expectedList = new ArrayList<>();
+
 
         expectedList.add(example1);
         expectedList.add(example3);
@@ -38,18 +40,22 @@ public class CompareSortingOrder {
 
     @Test
     public void compareSortingByModel(){
+
         Product example1 = new FoodProduct("ChinaFood","FriedDogChicks","10/02/2020","FastFood",7);
         Product example2 = new FoodProduct("Yamaika.Transition", "yellow-yellow", "12/12/2019", "ice-banana",90);
         Product example3 = new FoodProduct("Korea", "spice", "05/10/2018", "hot cake", 25);
         Product example4 = new FoodProduct("NashaRyaba", "chicken legs", "01/02/2020", "raw food", 100);
 
         ArrayList<Product> listForTest = new ArrayList<>();
-        ArrayList<Product> expectedList = new ArrayList<>();
 
         listForTest.add(example1);
         listForTest.add(example2);
         listForTest.add(example3);
         listForTest.add(example4);
+
+
+        ArrayList<Product> expectedList = new ArrayList<>();
+
 
         expectedList.add(example4);
         expectedList.add(example1);
